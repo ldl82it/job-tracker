0be77754bc41494d404581fa1bd3a0bf2e77e054
L <123399181+ldl82it@users.noreply.github.com>
2026-01-20 18:55:29 +0100

    feat: add application
    Handle form submit, validate inputs, update state and rerender list

--- js/main.js
+++ js/main.js
@@ -23,12 +23,58 @@ const ulCandidature = document.getElementById("applicationsList");
 
 const candidature = [candidatura1, candidatura2, candidatura3];
 
-candidature.forEach(function(candidaturaSingola) {
-    let stringaCandidatura = `${candidaturaSingola.company} - ${candidaturaSingola.role} - ${candidaturaSingola.status} - ${candidaturaSingola.date}`;
+const applicationForm = document.getElementById('applicationForm');
 
-    let liCandidatura = document.createElement("li");
-    liCandidatura.textContent = stringaCandidatura;
 
-    ulCandidature.appendChild(liCandidatura);
+function renderCandidature(){
 
-});
+    ulCandidature.innerHTML = "";
+
+    candidature.forEach(function(candidaturaSingola) {
+        let stringaCandidatura = `${candidaturaSingola.company} - ${candidaturaSingola.role} - ${candidaturaSingola.status} - ${candidaturaSingola.date}`;
+
+        let liCandidatura = document.createElement("li");
+        liCandidatura.textContent = stringaCandidatura;
+
+        ulCandidature.appendChild(liCandidatura);
+
+    });
+
+}
+/* PRIMO RENDER CANDIDATURE */
+renderCandidature();
+
+
+function submitApplicationForm(event){
+    event.preventDefault();
+
+    const form = event.target;
+
+    const company = form.company.value;
+    const role = form.role.value;
+    const status = form.status.value;
+    const date = form.date.value;
+
+    if ((company.trim().length === 0) || (role.trim().length === 0) || (status.trim().length === 0) || (date.trim().length === 0)){
+        alert("Riempi tutti i campi");
+        return;
+    }
+    
+    let nuovaCandidatura = {
+        company:company,
+        role:role,
+        status:status, 
+        date:date
+    }
+
+    candidature.push(nuovaCandidatura);
+
+    form.reset();
+
+    /* NUOOVO RENDER CANDIDATURE */
+    renderCandidature();
+}
+
+
+
+applicationForm.addEventListener('submit', submitApplicationForm);
